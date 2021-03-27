96cb6108fc5bfc92ea5f1b39f02925b671f6262a
danielkoren <daniels.korens@gmail.com>
2021-03-27 17:19:39 +0100

    Initial commit

---
+++ app/controllers/khachapuri_controller.rb
@@ -0,0 +1,7 @@
+class KhachapuriController < ApplicationController
+
+    def index
+        render :recipe 
+    end
+    
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  root 'khachapuri#index'
+
+end
